bd6f69b611b16585c456182fff5f715e9b467ee9
Anuj Pawar <anujpawar13@gmail.com>
2021-05-10 19:37:03 +0530

    addTxt.value now visible

--- app.js
+++ app.js
@@ -2,43 +2,23 @@ console.log('Welcome to EveryNotes');
 
 // adding a text when someone clicks add note
 
-// let addBtn = document.getElementById('addBtn');
-// addBtn.addEventListener("click", function(e){
-//     let addTxt = document.getElementById("addTxt");
-//     let notes = localStorage.getItem("notes");
-//     if(notes == null){
-//         notesObj = [0];
-//     }
-//     else{
-//         notesObj = JSON.parse(notes);
-//     }
-//     notesObj.push(addTxt.value);
-//     localStorage.setItem("notes", JSON.stringify(notesObj));
-//     // to save these notes in local storage as a string 
-
-//     addTxt.value = "";
-//     // to change the value to blank when someone clicks on add notes button
-
-//     // after this process, when notes are written and 'add notes' btn is presses, then this value gets stored in local storage 
-// })
-
-
-
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function(e){
-
     let addTxt = document.getElementById('addTxt');
     let notes = localStorage.getItem(addTxt.value);
-    let notesObj;
     if(notes == null){
         notesObj = [];
     }
     else{
         notesObj = JSON.parse(notes);
     }
-
     notesObj.push(addTxt.value);
     localStorage.setItem("notes", JSON.stringify(notesObj));
+    // to save these notes in local storage as a string 
+
     addTxt.value = "";
     console.log(notesObj);
-})
+    // to change the value to blank when someone clicks on add notes button
+
+    // after this process, when notes are written and 'add notes' btn is presses, then this value gets stored in local storage 
+});
